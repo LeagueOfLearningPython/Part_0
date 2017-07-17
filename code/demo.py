70fe5d73237bd88132fd502c598e392209bfb7ef
小青春 <875347754@qq.com>
2017-07-18 00:30:02 +0800

    modify code

--- code/demo.py
+++ code/demo.py
@@ -1,1 +1,10 @@
+#:声明Python解释器
+#:第一行告诉操作系统执行这个脚本的时候，调用/usr/bin下的python解释器,这相当于写死了python路径;
+#:第二行则会去环境设置寻找python目录,推荐这种写法
+#!/usr/bin/env python
+#!/usr/bin/python
+
+# 编码注释,这两个实际上是没有什么区别的，但是第二种写法比较好看
 #conding=utf-8
+#-*- coding: UTF-8 -*-
+
